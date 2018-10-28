87d9692ad26677d1a425699d8f640aaa2f40e478
Geoffrey Litt <glitt@panoramaed.com>
2018-10-28 14:42:48 -0400

    test

--- glitt/presentation.rb
+++ glitt/presentation.rb
@@ -38,92 +38,100 @@ end
 # Given an AST, return the evaluated result.
 # This is the heart of the language!
 def EVAL(ast, env)
-  if ast.is_a? Array
-    if ast.empty?
-      # Nothing to do to evaluate an empty list
-      ast
-    else
-      # This is our "apply" section -- where we interpret a list as
-      # a function applied to a list of arguments
-
-      # First, handle the *Special Forms* --
-
-      case ast.first
-
-      when :if
-        # Conditionals
-        conditional, true_branch, false_branch = ast[1..3]
-
-        if truthy?(EVAL(conditional, env))
-          return EVAL(true_branch, env)
-        else
-          return EVAL(false_branch, env)
-        end
-
-      when :def!
-        # Define a new value in the environment
-        # Example usage of def!:
-        # (def! a 6) ;=> 6
-        return env.set(ast[1], EVAL(ast[2], env))
-
-      when :"fn*"
-        # Function definition.
-        # Example usage:
-        # ( (fn* [a b] (+ a b)) 2 3 ) ;=> 5
-
-        return -> (*exprs) do
-          # Create a new environment with variables bound to the function args
-          new_env = Env.new(outer: env, binds: ast[1], exprs: exprs)
-
-          # Evaluate the function body in the context of that new environment
-          EVAL(ast[2], new_env)
-        end
-
-        # --------------------------
-        # Tail call friendly version
-        # --------------------------
-        # return {
-        #   ast: ast[2],
-        #   params: ast[1],
-        #   env: env
-        # }
+  loop do
+    if ast.is_a? Array
+      if ast.empty?
+        # Nothing to do to evaluate an empty list
+        ast
       else
-        # Finally, handle generic function application
-        evaluated = eval_ast(ast, env)
-        function, *args = evaluated
+        # This is our "apply" section -- where we interpret a list as
+        # a function applied to a list of arguments
 
-        # --------------------------
-        # Non Tail call friendly version
-        # --------------------------
-        return function.call(*args)
+        # First, handle the *Special Forms* --
 
-        # --------------------------
-        # Tail call friendly version
-        # --------------------------
+        case ast.first
 
-        # if function.is_a? Proc
-        #   return function.call(*args)
-        # # Handle user-defined functions in a tail-recursion-friendly way.
-        # elsif function.is_a? Hash
-        #   # Set ast to the function body, to prepare to evaluate it
-        #   # on our next loop iteration.
-        #   ast = function[:ast]
+        when :if
+          # Conditionals
+          conditional, true_branch, false_branch = ast[1..3]
 
-        #   # Replace the env with a new one with variables bound,
-        #   # we'll use it on our next loop iteration
-        #   env = Env.new(
-        #     outer: function[:env],
-        #     binds: function[:params],
-        #     exprs: args
-        #   )
-        # else
-        #   raise UndefinedFunctionError, "#{function} is not a function."
-        # end
+          if truthy?(EVAL(conditional, env))
+            ast = true_branch
+            next
+          else
+            ast = false_branch
+            next
+          end
+
+        when :def!
+          # Define a new value in the environment
+          # Example usage of def!:
+          # (def! a 6) ;=> 6
+          return env.set(ast[1], EVAL(ast[2], env))
+
+        when :"fn*"
+          # Function definition.
+          # Example usage:
+          # ( (fn* [a b] (+ a b)) 2 3 ) ;=> 5
+
+          params = ast[1]
+          fn_body = ast[2]
+
+          return -> (*arguments) do
+            # Create new environment with
+            # variables bound to arguments
+            new_env = Env.new(outer: env, binds: ast[1], exprs: arguments)
+
+            # Evaluate the function body in the context of that new environment
+            EVAL(ast[2], new_env)
+          end
+
+          # --------------------------
+          # Tail call friendly version
+          # --------------------------
+          return {
+            ast: ast[2],
+            params: ast[1],
+            env: env
+          }
+        else
+          # Finally, handle generic function application
+          evaluated = eval_ast(ast, env)
+          function, *args = evaluated
+
+          # --------------------------
+          # Non Tail call friendly version
+          # --------------------------
+          # return function.call(*args)
+
+          # --------------------------
+          # Tail call friendly version
+          # --------------------------
+
+          if function.is_a? Proc
+            return function.call(*args)
+          # Handle user-defined functions in a tail-recursion-friendly way.
+          elsif function.is_a? Hash
+            # Set ast to the function body, to prepare to evaluate it
+            # on our next loop iteration.
+            ast = function[:ast]
+
+            # Replace the env with a new one with variables bound,
+            # we'll use it on our next loop iteration
+            env = Env.new(
+              outer: function[:env],
+              binds: function[:params],
+              exprs: args
+            )
+          else
+            raise UndefinedFunctionError, "#{function} is not a function."
+          end
+        end
       end
+    else
+      # Atoms simply get resolved in the environment
+      return eval_ast(ast, env)
     end
-  else
-    # Atoms simply get resolved in the environment
-    return eval_ast(ast, env)
   end
 end
 
